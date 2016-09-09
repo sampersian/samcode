cc0b862951b4ea1d354bd91ecd4bc05975f8fed8
Samuel Persian <samuel.persian@gmail.com>
2016-09-09 09:35:10 -0600

    fixed the knexfile somehow

--- knexfile.js
+++ knexfile.js
@@ -26,11 +26,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL + '?ssl=true',
-      user:     process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD
-    },
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
